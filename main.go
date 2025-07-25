13c2dce750cd7ec22189e9a3ded4a295fc47dca5
Beibit05 <beibit.muzappar@narxoz.kz>
2025-07-25 10:45:03 +0500

    adaptation for main

--- main.go
+++ main.go
@@ -17,18 +17,6 @@ func main() {
 	fmt.Println("Hello World")
 	r := gin.Default()
 
-	r.GET("/users", handlers.GetUsers)
-	r.GET("/users/:id", handlers.GetByIdUser)
-	r.POST("/users", handlers.CreateUser)
-	r.PUT("/users/:id", handlers.UpdateUser)
-	r.DELETE("/users/:id", handlers.DeleteUser)
-
-	r.GET("/products", handlers.GetProduct)
-	r.GET("/products/:id", handlers.GetById)
-	r.POST("/products", handlers.CreateProduct)
-	r.PUT("/products/:id", handlers.UpdateProduct)
-	r.DELETE("/products/:id", handlers.DeleteProduct)
-
 	r.GET("/carts", handlers.GetCarts)
 	r.GET("/carts/:id", handlers.GetCartByID)
 	r.POST("/carts", handlers.CreateCart)
