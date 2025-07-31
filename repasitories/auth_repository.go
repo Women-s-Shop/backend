0d14b230ccb40a18af2678a0996845274228e64a
Beibit05 <beibit.muzappar@narxoz.kz>
2025-07-31 15:11:51 +0500

    add auth_repository

---
+++ repasitories/auth_repository.go
@@ -0,0 +1,28 @@
+package repasitories
+
+import (
+	"PracticalProject/models"
+	"gorm.io/gorm"
+)
+
+type AuthRepository interface {
+	CreateUser(user *models.User) error
+	GetByIdEmail(email string) (*models.User, error)
+}
+type authRepository struct {
+	db *gorm.DB
+}
+
+func NewAuthRepository(db *gorm.DB) AuthRepository {
+	return &authRepository{db}
+}
+func (r *authRepository) CreateUser(user *models.User) error {
+	err := r.db.Create(user).Error
+	return err
+}
+
+func (r *authRepository) GetByIdEmail(email string) (*models.User, error) {
+	var user models.User
+	err := r.db.Where("email=?", email).First(&user).Error
+	return &user, err
+}
