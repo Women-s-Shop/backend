d17190b4c43d2cfc4cb00af98de7de4957a45288
Beibit05 <beibit.muzappar@narxoz.kz>
2025-08-12 13:41:39 +0500

    updating orderitem handler,service,repo,routes

--- routes/orderItem_routes.go
+++ routes/orderItem_routes.go
@@ -1,17 +1,25 @@
 package routes
 
 import (
+	"PracticalProject/config"
 	"PracticalProject/handlers"
+	"PracticalProject/repositories"
+	"PracticalProject/services"
 	"github.com/gin-gonic/gin"
 )
 
 func InitOrderItemRoutes(r *gin.RouterGroup) {
+
+	OrderItemRepo := repositories.NewOrderItemRepository(config.DB)
+	OrderItemService := services.NewOrderItemService(OrderItemRepo)
+	OrderItemHandler := handlers.NewOrderItemHandler(OrderItemService)
+
 	orderItems := r.Group("/ordersItems")
 	{
-		orderItems.GET("/", handlers.GetOrderItems)
-		orderItems.GET("/:id", handlers.GetOrderItemByID)
-		orderItems.POST("/", handlers.CreateOrderItem)
-		orderItems.PUT("/:id", handlers.UpdateOrderItem)
-		orderItems.DELETE("/:id", handlers.DeleteOrderItem)
+		orderItems.GET("/", OrderItemHandler.GetOrderItems)
+		orderItems.GET("/:id", OrderItemHandler.GetOrderItemByID)
+		orderItems.POST("/", OrderItemHandler.CreateOrderItem)
+		orderItems.PUT("/:id", OrderItemHandler.UpdateOrderItem)
+		orderItems.DELETE("/:id", OrderItemHandler.DeleteOrderItem)
 	}
 }
